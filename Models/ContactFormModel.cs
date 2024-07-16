22c57e386071086d3f375dd8dbdc573e734fb147
Ryan Modisette <modisetteryan@gmail.com>
2024-07-16 17:23:36 -0400

    Added contact form model

---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Modisete.Models;
+public class Contact
+{
+    public int Id { get; set; }
+    [Required]
+    [Display(Name = "First Name")]
+    public string FirstName { get; set; }
+    [Required]
+    [Display(Name = "Last Name")]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Phone]
+    [Display(Name = "Phone Number")]
+    public string PhoneNumber { get; set; }
+    [Required]
+    public string Message { get; set; }
+    [Display(Name = "Time Submitted")]
+    public DateTime TimeSubmitted { get; set; }
+
+    public Contact()
+    {
+        TimeSubmitted = DateTime.UtcNow;
+    }
+
+}
